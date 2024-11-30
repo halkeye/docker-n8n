a101ea8f8883653e1a742fe632cb1cfb2844d1e1
Gavin Mogan <git@gavinmogan.com>
2024-11-30 00:50:03 -0800

    hooks

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
 ARG UPSTREAM_VERSION="1.69.2"
 FROM n8nio/n8n:$UPSTREAM_VERSION
 
-RUN mkdir /home/node/.n8n/nodes && \
-  cd /home/node/.n8n/nodes && \
-  npm install \
-  n8n-nodes-document-generator \
+WORKDIR /home/node/.n8n/nodes
+RUN npm install \
+  n8n-nodes-document-generator@1.0.9 \
   n8n-nodes-jwt@0.2.0 \
   n8n-nodes-webpage-content-extractor@0.1.2 \
   n8n-nodes-globals@1.0.3  \
   n8n-nodes-randomizer@0.1.0 \
   n8n-nodes-searxng@0.2.5 \
-  @muench-dev/n8n-nodes-bluesky@2.2.0 \
-  n8n-nodes-pdfkit@0.1.2 
+  @muench-dev/n8n-nodes-bluesky@2.2.0
 
+WORKDIR /home/node/.n8n/hooks/
+ENV EXTERNAL_HOOK_FILES=/home/node/.n8n/hooks/autologin.js
+COPY hooks/ /home/node/.n8n/hooks/
 
-# USER 1000
+WORKDIR /home/node
---
+++ hooks/autologin.js
@@ -0,0 +1,67 @@
+// From https://community.n8n.io/t/self-hosted-user-management/30520
+const {dirname, resolve} = require('path')
+const Layer = require('express/lib/router/layer')
+const basicAuth = require('basic-auth')
+const {issueCookie} = require(resolve(dirname(require.resolve('n8n')), 'auth/jwt'))
+
+const basicAuthCredentials = {
+  username: process.env.N8N_BASIC_AUTH_USER,
+  password: process.env.N8N_BASIC_AUTH_PASSWORD,
+}
+
+const ignoreAuthRegexp = /^\/(assets|healthz|webhook)/;
+
+module.exports = {
+  n8n: {
+    ready: [
+      async function ({app}, config) {
+        await this.dbCollections.Settings.update(
+          {key: 'userManagement.isInstanceOwnerSetUp'},
+          {value: JSON.stringify(true)},
+        )
+
+        config.set('userManagement.isInstanceOwnerSetUp', true)
+
+        const {stack} = app._router;
+
+        if (basicAuthCredentials.username && basicAuthCredentials.password) {
+          stack.unshift(new Layer('/', {
+            strict: false,
+            end: false
+          }, async (req, res, next) => {
+            if (ignoreAuthRegexp.test(req.url)) return next();
+
+            const authorization = basicAuth(req);
+            if (!authorization || authorization.name !== basicAuthCredentials.username || authorization.pass !== basicAuthCredentials.password) {
+              res.statusCode = 401
+              res.setHeader('WWW-Authenticate', 'Basic realm="n8n"')
+              res.end('Access denied')
+            } else {
+              next()
+            }
+          }))
+        }
+
+        const index = stack.findIndex((l) => l.name === 'cookieParser')
+        stack.splice(index + 1, 0, new Layer('/', {
+          strict: false,
+          end: false
+        }, async (req, res, next) => {
+          if (!req.cookies?.['n8n-auth']) {
+            const owner = await this.dbCollections.User.findOneBy({
+              globalRole: {
+                name: 'owner',
+                scope: 'global',
+              },
+            })
+            issueCookie(res, owner)
+          }
+
+          next()
+        }))
+
+        console.log("UM Disabled")
+      },
+    ],
+  },
+}
